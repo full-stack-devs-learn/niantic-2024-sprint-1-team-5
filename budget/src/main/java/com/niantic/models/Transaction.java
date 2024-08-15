7a7f8f0827728b7c598518c47218066fa8a7da1d
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-08-15 10:23:49 -0700

    Created transaction model

--- budget/src/main/java/com/niantic/models/Transaction.java
+++ budget/src/main/java/com/niantic/models/Transaction.java
@@ -1,5 +1,97 @@
 package com.niantic.models;
 
-public class Transaction {
-    Private
+import jdk.jfr.Category;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Transaction
+{
+    private int transaction_id;
+    private int owner_id;
+    private int budget_id;
+    private int vendor_id;
+    private int subcategory_id;
+    private double amount;
+    private LocalDate date;
+    private String note;
+
+    public Transaction() {
+
+    }
+    public Transaction(int transaction_id, int owner_id, int budget_id, int vendor_id, int subcategory_id, double amount, LocalDate date, String note)
+    {
+        this.transaction_id = transaction_id;
+        this.owner_id = owner_id;
+        this.budget_id = budget_id;
+        this.vendor_id = vendor_id;
+        this.subcategory_id = subcategory_id;
+        this.amount = amount;
+        this.date = date;
+        this.note = note;
+    }
+
+    public int getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setTransaction_id(int transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    public int getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(int owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public int getBudget_id() {
+        return budget_id;
+    }
+
+    public void setBudget_id(int budget_id) {
+        this.budget_id = budget_id;
+    }
+
+    public int getVendor_id() {
+        return vendor_id;
+    }
+
+    public void setVendor_id(int vendor_id) {
+        this.vendor_id = vendor_id;
+    }
+
+    public int getSubcategory_id() {
+        return subcategory_id;
+    }
+
+    public void setSubcategory_id(int subcategory_id) {
+        this.subcategory_id = subcategory_id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
